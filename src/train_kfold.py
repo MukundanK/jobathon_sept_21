c827951dc2a49dfda2f104e41bb2957c2daaa1ac
Kavya Mukundan <mukundank@Kavyas-MacBook-Pro.local>
2021-09-17 21:32:57 -0400

    added script to do kfold split

---
+++ src/train_kfold.py
@@ -0,0 +1,33 @@
+
+# import
+import pandas as pd
+import config
+import os
+from sklearn import model_selection
+
+def create_folds(df, target_col, processed_dir):
+
+    # create kfold column and fill with 0
+    df['kfold'] = 0
+
+    # shuffle data and reset index
+    df = df.sample(frac = 1).reset_index(drop = True)
+
+    # kfold
+    kf = model_selection.KFold(n_splits=3)
+    for f, (t_, v_) in enumerate(kf.split(X = df, y = df[target_col].values)):
+        df.loc[v_, 'kfold'] = f
+
+    output_file = os.path.join(processed_dir, 'train_kfold.csv')
+    df.to_csv(output_file, index = None)
+
+    return None
+
+if __name__ == '__main__':
+
+    data = config.TRAIN
+    target_col = config.TARGET
+    processed_dir = config.PROCESSED
+
+    train = pd.read_csv(data)
+    create_folds(train, target_col, processed_dir)
