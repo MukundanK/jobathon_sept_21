3c5501a460cda104ee4dfc150448db237ba8f7b7
Kavya Mukundan <mukundank@Kavyas-MacBook-Pro.local>
2021-09-19 16:11:07 -0400

    initial commit

---
+++ src/config.py
@@ -0,0 +1,10 @@
+INPUT = '../data/input'
+PROCESSED = '../data/processed'
+OUTPUT = '../data/output'
+
+TRAIN = '../data/input/TRAIN.csv'
+TRAIN_KFOLD = '../data/processed/train_kfold.csv'
+TEST = '../data/input/TEST_FINAL.csv'
+
+TARGET = 'Sales'
+DATE = 'Date'
